6765f289790416f6ce92374fbc4b43e2d2366997
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-09-15 01:24:31 +0530

    *: unshare

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub enum ErrorCode {
+    Unshare,
+}
+
+pub mod namespace_flags {
+    pub use libc::{
+        CLONE_NEWCGROUP as CGroup, CLONE_NEWIPC as Ipc, CLONE_NEWNET as Net, CLONE_NEWNS as Mount,
+        CLONE_NEWPID as Pid, CLONE_NEWUSER as User, CLONE_NEWUTS as Uts,
+    };
+}
+
+fn errno() -> std::io::Error {
+    std::io::Error::last_os_error()
+}
+
+pub unsafe fn unshare(flags: i32) -> Result<(), std::io::Error> {
+    if libc::unshare(flags) == -1 {
+        return Err(errno());
+    }
+
+    Ok(())
+}
