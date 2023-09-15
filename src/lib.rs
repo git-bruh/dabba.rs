781ee901849df5d57b85b2a3635e0346b86f4eed
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-09-15 21:07:22 +0530

    *: more setup helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,81 @@
-pub mod namespace_flags {
-    pub use libc::{
-        CLONE_NEWCGROUP as CGroup, CLONE_NEWIPC as Ipc, CLONE_NEWNET as Net, CLONE_NEWNS as Mount,
-        CLONE_NEWPID as Pid, CLONE_NEWUSER as User, CLONE_NEWUTS as Uts,
+use nix::mount::{MntFlags, MsFlags};
+use nix::sched::CloneFlags;
+use std::fs::File;
+use std::io::Write;
+
+/// Prevent ourselves from gaining any further privileges, say
+/// through executing setuid programs like `sudo` or `doas`
+pub fn no_new_privs() -> nix::Result<()> {
+    nix::sys::prctl::set_no_new_privs()
+}
+
+/// Pretent to be root inside the container by creating the appropriate
+/// UID and GID mappings
+pub fn root() -> Result<(), std::io::Error> {
+    const MAPPING: &[u8] = b"0 1000 1";
+
+    let write = |sub_file, contents| -> Result<(), std::io::Error> {
+        let mut file = File::create(format!("/proc/self/{sub_file}"))?;
+        file.write_all(contents)?;
+
+        Ok(())
     };
-}
 
-fn errno() -> std::io::Error {
-    std::io::Error::last_os_error()
-}
+    write("uid_map", MAPPING)?;
 
-pub fn unshare(flags: i32) -> Result<(), std::io::Error> {
-    unsafe {
-        if libc::unshare(flags) == -1 {
-            return Err(errno());
-        }
-    }
+    /* Unprivileged writes to `gid_map` are not possible unless
+     * we disable setgroups() completely */
+    write("setgroups", b"deny")?;
+
+    write("gid_map", MAPPING)?;
 
     Ok(())
+}
+
+/// Set up the namespace
+pub fn unshare() -> nix::Result<()> {
+    nix::sched::unshare(
+        CloneFlags::CLONE_NEWNS
+            | CloneFlags::CLONE_NEWUSER
+            | CloneFlags::CLONE_NEWPID
+            | CloneFlags::CLONE_NEWNET
+            | CloneFlags::CLONE_NEWIPC
+            | CloneFlags::CLONE_NEWUTS
+            | CloneFlags::CLONE_NEWCGROUP,
+    )
+}
+
+/// Mounting dance
+pub fn mounts(container_src: &str) -> nix::Result<()> {
+    // Shuts up the type annotation errors caused by
+    // nix::mount::mount trait bounds
+    let null = None::<&str>;
+
+    // Prevent mounts from propagating outside/into the namespace
+    nix::mount::mount(null, "/", null, MsFlags::MS_REC | MsFlags::MS_PRIVATE, null)?;
+
+    // Bind mount the container to a new directory for pivot_root()-ing
+    // Must mount recursively as we could otherwise access a masked directory
+    // inside a container
+    nix::mount::mount(
+        Some(container_src),
+        "/tmp",
+        null,
+        MsFlags::MS_REC | MsFlags::MS_BIND,
+        null,
+    )?;
+
+    nix::unistd::chdir("/tmp")?;
+
+    // pivot_root() without creating an intermediate directory, as
+    // described in `pivot_root(2)` NOTES section
+    nix::unistd::pivot_root(".", ".")?;
+    nix::mount::umount2(".", MntFlags::MNT_DETACH)?;
+
+    Ok(())
+}
+
+/// Create a new "session",
+pub fn new_session() -> nix::Result<nix::unistd::Pid> {
+    nix::unistd::setsid()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use rustainer::{namespace_flags, unshare};
+use rustainer::{mounts, no_new_privs, root, unshare};
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    unshare(
-        namespace_flags::Mount
-            | namespace_flags::User
-            | namespace_flags::Pid
-            | namespace_flags::Net
-            | namespace_flags::Ipc
-            | namespace_flags::Uts
-            | namespace_flags::CGroup,
-    )?;
+    // TODO
+    //   - Hostname
+    //   - Mounting new pseudo-filesystems at /proc, /dev, /sys
+
+    println!("prctl(PR_SET_NO_NEW_PRIVS)");
+    no_new_privs()?;
+
+    println!("Setting up namespace");
+    unshare()?;
+
+    println!("Setting up UID and GID mappings");
+    root()?;
+
+    println!("Performing the mounting dance");
+    mounts("/")?;
 
     Command::new("sh").status()?;
     Ok(())
