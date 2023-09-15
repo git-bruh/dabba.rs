cc7494b47dfbab3132c0873e87ca318b824cd16c
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-09-15 13:28:00 +0530

    *: expose safe function with unsafe block

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-pub enum ErrorCode {
-    Unshare,
-}
-
 pub mod namespace_flags {
     pub use libc::{
         CLONE_NEWCGROUP as CGroup, CLONE_NEWIPC as Ipc, CLONE_NEWNET as Net, CLONE_NEWNS as Mount,
@@ -13,9 +9,11 @@ fn errno() -> std::io::Error {
     std::io::Error::last_os_error()
 }
 
-pub unsafe fn unshare(flags: i32) -> Result<(), std::io::Error> {
-    if libc::unshare(flags) == -1 {
-        return Err(errno());
+pub fn unshare(flags: i32) -> Result<(), std::io::Error> {
+    unsafe {
+        if libc::unshare(flags) == -1 {
+            return Err(errno());
+        }
     }
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,15 @@ use rustainer::{namespace_flags, unshare};
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    unsafe {
-        unshare(
-            namespace_flags::Mount
-                | namespace_flags::User
-                | namespace_flags::Pid
-                | namespace_flags::Net
-                | namespace_flags::Ipc
-                | namespace_flags::Uts
-                | namespace_flags::CGroup,
-        )?;
-    }
+    unshare(
+        namespace_flags::Mount
+            | namespace_flags::User
+            | namespace_flags::Pid
+            | namespace_flags::Net
+            | namespace_flags::Ipc
+            | namespace_flags::Uts
+            | namespace_flags::CGroup,
+    )?;
 
     Command::new("sh").status()?;
     Ok(())
