225eea457dcec2d91f2389a0bb42088e46bef08a
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-09-22 01:44:49 +0530

    mount_helper: setup a /dev hierarchy

--- src/mount_helper.rs
+++ src/mount_helper.rs
@@ -1,5 +1,7 @@
 use nix::mount::{MntFlags, MsFlags};
-use std::path::Path;
+use std::fs::OpenOptions;
+use std::os::unix::fs::{symlink, OpenOptionsExt};
+use std::path::{Path, PathBuf};
 
 // Shuts up the type annotation errors caused by nix::mount::mount trait bounds
 const NULL: Option<&str> = None;
@@ -7,7 +9,6 @@ const NULL: Option<&str> = None;
 #[derive(Debug)]
 pub enum MountType {
     Dev,
-    DevPts,
     Proc,
     Sys,
     Tmp,
@@ -19,8 +20,7 @@ pub fn block_mount_propagation() -> nix::Result<()> {
 }
 
 /// Bind mount the container to a new directory for pivot_root()-ing
-/// Must mount recursively as we could otherwise access a masked directory
-/// inside a container
+/// TODO remove this in another refactor
 pub fn bind_container(container: &Path, target: &Path) -> nix::Result<()> {
     nix::mount::mount(
         Some(container),
@@ -31,19 +31,95 @@ pub fn bind_container(container: &Path, target: &Path) -> nix::Result<()> {
     )
 }
 
+/// Bind `from_path` to `to_path`
+/// Must mount recursively as we could otherwise access a masked directory
+/// inside a container
+pub fn bind(
+    from_path: &Path,
+    to_path: &Path,
+    flags: Option<MsFlags>,
+) -> Result<(), std::io::Error> {
+    log::info!("Binding {from_path:?} to {to_path:?}");
+
+    // TODO support directories
+    OpenOptions::new()
+        .write(true)
+        // Read Only, doesn't matter as the bind mount sets permissions
+        .mode(0o444)
+        .create(true)
+        .open(to_path)?;
+
+    nix::mount::mount(
+        Some(from_path),
+        to_path,
+        NULL,
+        MsFlags::MS_REC | MsFlags::MS_BIND | flags.unwrap_or(MsFlags::empty()),
+        NULL,
+    )?;
+
+    Ok(())
+}
+
 pub fn perform_pseudo_fs_mount(mount: MountType, path: &Path) -> Result<(), std::io::Error> {
     log::info!("Mounting {mount:?} at {path:?}");
     std::fs::create_dir_all(path)?;
 
     match mount {
-        MountType::Dev => nix::mount::mount(
-            Some("dev"),
-            path,
-            Some("tmpfs"),
-            MsFlags::MS_NOSUID | MsFlags::MS_NOEXEC | MsFlags::MS_NODEV,
-            NULL,
-        )?,
-        MountType::DevPts => panic!("TODO"),
+        MountType::Dev => {
+            nix::mount::mount(
+                Some("dev"),
+                path,
+                Some("tmpfs"),
+                MsFlags::MS_NOSUID | MsFlags::MS_NOEXEC | MsFlags::MS_NODEV,
+                // Must mount the tmpfs as read-write so that bind mounts of
+                // character devices can be written to
+                Some("mode=755"),
+            )?;
+
+            // Set up a traditional /dev hierarchy
+            let host_dev_base = PathBuf::from("/dev");
+            let container_dev_base = PathBuf::from(path);
+
+            // We cannot call mknod() as an unprivileged user for creating
+            // all these special files
+            for node in ["full", "null", "random", "tty", "urandom", "zero"] {
+                bind(
+                    &host_dev_base.join(node),
+                    &container_dev_base.join(node),
+                    None,
+                )?;
+            }
+
+            // Standard symlinks
+            log::info!("Creating /dev stdio symlinks");
+            for (fd, node) in ["stdin", "stdout", "stderr"].iter().enumerate() {
+                symlink(format!("/proc/self/fd/{fd}"), container_dev_base.join(node))?;
+            }
+
+            // Dummy directory for shared memory
+            log::info!("Creating shm");
+            std::fs::create_dir(container_dev_base.join("shm"))?;
+
+            // Pseudo terminal devices
+            log::info!("Creating ptmx symlink");
+            symlink("pts/ptmx", container_dev_base.join("ptmx"))?;
+
+            log::info!("Mounting devpts");
+
+            let pts = container_dev_base.join("pts");
+
+            std::fs::create_dir(pts)?;
+            nix::mount::mount(
+                Some("devpts"),
+                &container_dev_base.join("pts"),
+                Some("devpts"),
+                MsFlags::MS_NOSUID | MsFlags::MS_NOEXEC,
+                // See "Mount options for devpts" in the mount(8) man-page
+                // Create a private devpts instance and make it RW, but not
+                // executable by all
+                Some("newinstance,ptmxmode=0666"),
+            )?
+        }
         MountType::Proc => nix::mount::mount(
             Some("proc"),
             path,
