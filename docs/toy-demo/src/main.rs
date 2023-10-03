2d91ab26105b894cb0d9e49d6e9f4fa93e61ec84
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-10-03 22:37:44 +0530

    docs/toy-demo: uid gid 0

--- docs/toy-demo/src/main.rs
+++ docs/toy-demo/src/main.rs
@@ -2,6 +2,9 @@ use nix::mount::MsFlags;
 use nix::sched::CloneFlags;
 use nix::sys::signal::Signal;
 use std::ffi::CString;
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::Write;
 
 fn in_sandbox() -> isize {
     let argv: [CString; 1] = [CString::new("sh").unwrap()];
@@ -19,6 +22,33 @@ fn in_sandbox() -> isize {
     nix::unistd::execve(&CString::new("/bin/sh").unwrap(), &argv, &env).expect("failed to exec!");
 
     0
+}
+
+fn make_root(pid: i32) -> Result<(), std::io::Error> {
+    // /proc/<pid>
+    let mut path = PathBuf::new();
+
+    path.push("/proc");
+    path.push(pid.to_string());
+
+    // We must never allow setgroups() to be called as that could potentially
+    // allow access to otherwise restricted resources
+    let mut setgroups = File::create(path.join("setgroups"))?;
+    setgroups.write_all(b"deny")?;
+
+    println!("Mapping UID 1000 to 0 (root) in sandbox");
+
+    // /proc/<pid>/uid_map
+    let mut uid_map = File::create(path.join("uid_map"))?;
+    uid_map.write_all(b"0 1000 1")?;
+
+    println!("Mapping GID 1000 to 0 (root) in sandbox");
+
+    // /proc/<pid>/gid_map
+    let mut gid_map = File::create(path.join("gid_map"))?;
+    gid_map.write_all(b"0 1000 1")?;
+
+    Ok(())
 }
 
 fn main() {
@@ -41,6 +71,10 @@ fn main() {
         )
         .expect("failed to clone!")
     };
+
+    println!("Spawned sandbox with PID: {pid}");
+
+    make_root(pid.as_raw()).expect("failed to make uid 0!");
 
     nix::sys::wait::waitpid(pid, None).expect("failed to wait!");
 }
